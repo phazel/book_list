7712d81c3e78652b2739a3d4a847ad83123cbba4
phazel <courtney.p.hazel@gmail.com>
2018-12-23 13:55:26 +1100

    Refactor

--- convert.rb
+++ convert.rb
@@ -4,6 +4,25 @@ require 'json'
 
 YEAR = '2018'
 
+hash = JSON.load File.read("#{YEAR}/exported.json")
+
+read_list = hash['lists'].find{|list| list['name'] == "Read #{YEAR}" }
+read_books = hash['cards'].select{|card| card['idList'] == read_list['id'] }
+
+not_finished_label = hash['labels'].find{|label| label['name'] == 'Not Gonna Finish'}
+not_finished_books = []
+
+output = read_books.map do |book|
+  not_finished = book['idLabels'].include? not_finished_label['id']
+  not_finished_books << book if not_finished
+  not_finished ? "" : format(book)
+end
+
+output << "BOOKS I DIDN'T FINISH:\n\n"
+output << not_finished_books.map{ |book| format(book) }
+
+File.write "#{YEAR}/books_read_#{YEAR}.md", output.join
+
 def format(book)
   <<~SUMMARY
   **#{book['name']}**
@@ -11,29 +30,3 @@ def format(book)
 
   SUMMARY
 end
-
-hash = JSON.load File.read("#{YEAR}/exported.json")
-
-not_finished_label = hash['labels'].find{|label| label['name'] == 'Not Gonna Finish'}
-
-read_list = hash['lists'].find{|list| list['name'] == "Read #{YEAR}" }
-read_books = hash['cards'].select{|card| card['idList'] == read_list['id'] }
-
-not_finished_books = []
-
-output = read_books.map do |book|
-  if book['idLabels'].include? not_finished_label['id']
-    not_finished_books << book
-    ""
-  else
-    format book
-  end
-end
-
-output << "BOOKS I DIDN'T FINISH:\n\n"
-
-output << not_finished_books.map do |book|
-  format book
-end
-
-File.write "#{YEAR}/books_read_#{YEAR}.md", output.join
