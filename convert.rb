8245f4c6dcdb9c91f6e61e9da615d1193d784823
phazel <courtney.p.hazel@gmail.com>
2019-12-21 12:56:33 +1100

    Move present method back to convert script

--- convert.rb
+++ convert.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 require 'json'
-require_relative './lib/books'
 require_relative './lib/find'
 require_relative './lib/filter'
 
@@ -9,6 +8,16 @@ READ_LIST = "Read #{YEAR}"
 CURRENTLY_READING_LIST = "*** Reading ***"
 NOT_FINISHING_LABEL = 'Not Gonna Finish'
 FAVOURITE_LABEL = 'fav'
+
+def present(books)
+  books.map do |book|
+    <<~SUMMARY
+    **#{book['name']}**
+    *by #{book['desc']}*
+
+    SUMMARY
+  end
+end
 
 hash = JSON.load File.read("#{YEAR}/exported.json")
 
@@ -21,21 +30,21 @@ output << "`Total books read: #{read_books.size}`\n\n\n"
 
 output << "## Favourites:\n\n"
 favourites = Filter.with_label(read_books, favourite)
-output << Books.present(favourites)
+output << present(favourites)
 output << "---\n\n"
 
 regular_read_books = Filter.without_labels(read_books, [favourite, not_finishing])
-output << Books.present(regular_read_books)
+output << present(regular_read_books)
 output << "---\n\n"
 
 not_finishing_books = Filter.with_label(read_books, not_finishing)
 if not_finishing_books.any?
   output << "## Books I Decided Not To Finish:\n\n"
-  output << Books.present(not_finishing_books)
+  output << present(not_finishing_books)
   output << "---\n\n"
 end
 
 output << "## Books I'm Currently Reading:\n\n"
-output << Books.present(Find.books_in_list(hash, CURRENTLY_READING_LIST))
+output << present(Find.books_in_list(hash, CURRENTLY_READING_LIST))
 
 File.write "#{YEAR}/books_read_#{YEAR}.md", output.join
