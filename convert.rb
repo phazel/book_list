dc32b5ab8ec962046ba3974963db8f05b08e5766
phazel <courtney.p.hazel@gmail.com>
2019-12-21 14:39:37 +1100

    Refactor Format class

--- convert.rb
+++ convert.rb
@@ -16,8 +16,8 @@ favourite_label = Find.label(hash, FAVOURITE_LABEL)
 not_finishing_label = Find.label(hash, NOT_FINISHING_LABEL)
 read_list = Find.list(hash, READ_LIST)
 currently_reading_list = Find.list(hash, CURRENTLY_READING_LIST)
-
 books = hash['cards']
+
 read = Filter.in_list(books, read_list)
 currently_reading = Filter.in_list(books, currently_reading_list)
 favourites = Filter.with_label(read, favourite_label)
@@ -25,9 +25,9 @@ regular_read = Filter.without_labels(read, [favourite_label, not_finishing_label
 not_finishing = Filter.with_label(read, not_finishing_label)
 
 output = Format.header(YEAR, read.size)
-output += Format.favourites(favourites)
-output += Format.regular_read(regular_read)
-output += Format.not_finishing(not_finishing) if not_finishing.any?
-output += Format.currently_reading(currently_reading)
+output += Format.section(favourites, :favourites)
+output += Format.section(regular_read)
+output += Format.section(not_finishing, :not_finishing) if not_finishing.any?
+output += Format.section(currently_reading, :currently_reading)
 
 File.write "#{YEAR}/books_read_#{YEAR}.md", output.join
--- lib/format.rb
+++ lib/format.rb
@@ -1,6 +1,23 @@
 require 'pry'
 
 class Format
+  SECTION_HEADERS = {
+    favourites: "## Favourites:\n\n",
+    not_finishing: "## Books I Decided Not To Finish:\n\n",
+    currently_reading: "## Books I'm Currently Reading:\n\n"
+  }
+
+  def self.header(year, number)
+    [
+      "# Books Read In #{year}\n",
+      "`Total books read: #{number}`\n\n"
+    ]
+  end
+
+  def self.section(books, header = nil)
+    [ line, SECTION_HEADERS[header], pretty(books) ]
+  end
+
   def self.pretty(books)
     books.map do |book|
       <<~SUMMARY
@@ -9,43 +26,6 @@ class Format
 
       SUMMARY
     end
-  end
-
-  def self.header(year, number)
-    [
-      "# Books Read In #{year}\n",
-      "`Total books read: #{number}`\n\n\n"
-    ]
-  end
-
-  def self.favourites(books)
-    [
-      "## Favourites:\n\n",
-      pretty(books),
-      line
-    ]
-  end
-
-  def self.regular_read(books)
-    [
-      pretty(books),
-      line
-    ]
-  end
-
-  def self.not_finishing(books)
-    [
-      "## Books I Decided Not To Finish:\n\n",
-      pretty(books),
-      line
-    ]
-  end
-
-  def self.currently_reading(books)
-    [
-      "## Books I'm Currently Reading:\n\n",
-      pretty(books)
-    ]
   end
 
   def self.line
