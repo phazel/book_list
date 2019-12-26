65175734352ab73f138af819b3e5c44cba3f570f
phazel <courtney.p.hazel@gmail.com>
2019-12-26 14:56:24 +1100

    Add more Format specs

--- spec/format_spec.rb
+++ spec/format_spec.rb
@@ -2,25 +2,69 @@ require 'format'
 
 audiobook_label = { "id"=>"a_id", "name"=>"audiobook" }
 
-book_with_no_labels = { "idLabels"=>[], "labels"=> [] }
-book_with_audiobook_label = { "idLabels"=>[ audiobook_label["id"] ], "labels"=> [ audiobook_label ] }
+book_with_no_labels = { "idLabels"=>[],
+  "labels"=> [],
+  "name"=>'book_1',
+  "desc"=>'Author 1'
+}
+book_with_audiobook_label = {
+  "idLabels"=>[ audiobook_label["id"] ],
+  "labels"=> [ audiobook_label ],
+  "name"=> 'book_2',
+  "desc"=> 'Author 2'
+}
 
 hash = {
   'labels' => [ audiobook_label ],
   'cards' => [ book_with_no_labels, book_with_audiobook_label ]
 }
 
-book_not_audiobook = { "idLabels"=>[], "labels"=> [], "audiobook"=> false }
+book_not_audiobook = { "idLabels"=>[],
+  "labels"=> [],
+  "name"=>'book_1',
+  "desc"=>'Author 1',
+  "audiobook"=> false
+}
 book_is_audiobook = {
   "idLabels"=>[ audiobook_label["id"] ],
   "labels"=> [ audiobook_label ],
+  "name"=> 'book_2',
+  "desc"=> 'Author 2',
   "audiobook"=> true
 }
 
 books_with_audiobook_flag = [ book_not_audiobook, book_is_audiobook ]
 
+pretty_books = [
+  "**book_1**\n*by Author 1*\n\n",
+  "**book_2** (Audiobook)\n*by Author 2*\n\n"
+]
+
+
 describe Format do
+  describe '.header' do
+    let(:year) { 3904 }
+    let(:number) { 45 }
+    let(:expected_header) {[ "# Books Read In 3904\n", "`Total books read: 45`\n\n" ]}
+    it { expect(described_class.header(year, number)).to eq expected_header }
+  end
+
+  describe '.section' do
+    let(:expected_section) {
+      [ "---\n\n", "## Favourites:\n\n", pretty_books ]
+    }
+    it { expect(described_class.section(books_with_audiobook_flag, :favourites)) }
+  end
+
+  describe '.pretty' do
+    it { expect(described_class.pretty(books_with_audiobook_flag)).to eq pretty_books }
+  end
+
   describe '.add_audiobook' do
     it { expect(described_class.add_audiobook(hash, audiobook_label)).to eq books_with_audiobook_flag }
+  end
+
+  describe '.line' do
+    it { expect(described_class.line).to eq [ "---\n\n" ] }
   end
 end
