283e484ec7be316c28635752114d0bd17c5ca552
Hazel Garcia <hazelpetagarcia@gmail.com>
2021-12-04 13:55:28 +1100

    Separate run.rb script file and app.rb file with generate function

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Reads a Trello json export and creates a markdown file of all the books I read in a particular year. I then use this as a template for a blog post at the end of each year.
 
-The year is hardcoded in the `generate.rb` script. What ya gonna do about it? 🤷🏻
+The year is hardcoded in the `run.rb` script.
 
-Run: `bundle exec generate.rb`
+Run: `bundle exec run.rb`
---
+++ lib/app.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'json'
+require_relative './extract'
+require_relative './filter'
+require_relative './format'
+
+class App
+  def self.generate(year)
+    hash = Format.strip Format.symbify JSON.parse File.read "#{year}/exported.json"
+    File.open("#{year}/exported_pretty.json", 'w') do |file|
+      file.write JSON.pretty_generate hash
+    end
+
+    all_books = Extract.all_books(hash, year)
+    read, current = Filter.by_list(all_books).values_at(:read, :current)
+    dups, non_dups = Filter.duplicates(read).values_at(:dups, :non_dups)
+
+    sections = {
+      count: Filter.without(read, [:dnf]).size,
+      dups: dups,
+      fav: Filter.with(non_dups, :fav),
+      regular: Filter.without(non_dups, %i[fav dnf]),
+      dnf: Filter.with(read, :dnf)
+    }
+
+    output = Format.result year, sections, current
+    File.write "#{year}/books_read_#{year}.md", output.join
+
+    {
+      count: sections[:count],
+      fav: sections[:fav].size,
+      dups: dups.size,
+      nat: Filter.with(read, :nat).size,
+      sleep: Filter.with(read, :sleep).size,
+      audiobook: Filter.with(read, :audiobook).size,
+      ebook: Filter.with(read, :ebook).size,
+      physical: Filter.without(read, [:audiobook, :ebook]).size,
+      dnf: sections[:dnf].size,
+      current: current.size,
+    }
+  end
+end
