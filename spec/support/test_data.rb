c0862d75fef1cd9a120427b2bc2738a3badba432
Hazel Garcia <hazelpetagarcia@gmail.com>
2021-12-30 15:51:12 +1100

    Freeze test data

--- spec/support/test_data.rb
+++ spec/support/test_data.rb
@@ -15,14 +15,14 @@ module TestData
     Shaping the Fractured Self: Poetry of Chronic Illness and Pain,Heather Taylor Johnson (Editor),physical,Paused
   BOOKS
 
-  DUNE_HASH = { title: 'Dune', author: 'Frank Herbert', status: 'current', formats: ['audiobook'] }
-  WOLF_HALL_HASH = { title: 'Wolf Hall', author: 'Hilary Mantel', status: 'done', formats: ['audiobook'] }
-  STOIC_HASH = { title: 'Letters From a Stoic', author: 'Seneca', status: 'done', formats: ['audiobook'] }
-  OTHER_PARTIES_HASH = { title: 'Her Body and Other Parties', author: 'Carmen Maria Machado', status: 'current', formats: ['physical', 'read aloud'] }
-  IF_FORTUNATE_HASH = { title: 'To Be Taught, If Fortunate', author: 'Becky Chambers', status: 'done', formats: ['physical', 'read aloud'] }
-  WE_HASH = { title: 'We', author: 'Yevgeny Zamyatin', status: 'To Read', formats: ['audiobook', 'ebook', 'physical'] }
-  FRACTURED_SELF_HASH = { title: 'Shaping the Fractured Self: Poetry of Chronic Illness and Pain', author: 'Heather Taylor Johnson (Editor)', status: 'Paused', formats: ['physical'] }
-  HASHES = [DUNE_HASH, WOLF_HALL_HASH, STOIC_HASH, OTHER_PARTIES_HASH, IF_FORTUNATE_HASH, WE_HASH, FRACTURED_SELF_HASH]
+  DUNE_HASH = { title: 'Dune', author: 'Frank Herbert', status: 'current', formats: ['audiobook'] }.freeze
+  WOLF_HALL_HASH = { title: 'Wolf Hall', author: 'Hilary Mantel', status: 'done', formats: ['audiobook'] }.freeze
+  STOIC_HASH = { title: 'Letters From a Stoic', author: 'Seneca', status: 'done', formats: ['audiobook'] }.freeze
+  OTHER_PARTIES_HASH = { title: 'Her Body and Other Parties', author: 'Carmen Maria Machado', status: 'current', formats: ['physical', 'read aloud'] }.freeze
+  IF_FORTUNATE_HASH = { title: 'To Be Taught, If Fortunate', author: 'Becky Chambers', status: 'done', formats: ['physical', 'read aloud'] }.freeze
+  WE_HASH = { title: 'We', author: 'Yevgeny Zamyatin', status: 'To Read', formats: ['audiobook', 'ebook', 'physical'] }.freeze
+  FRACTURED_SELF_HASH = { title: 'Shaping the Fractured Self: Poetry of Chronic Illness and Pain', author: 'Heather Taylor Johnson (Editor)', status: 'Paused', formats: ['physical'] }.freeze
+  HASHES = [DUNE_HASH, WOLF_HALL_HASH, STOIC_HASH, OTHER_PARTIES_HASH, IF_FORTUNATE_HASH, WE_HASH, FRACTURED_SELF_HASH].freeze
 
   DUNE = hash_to_book(DUNE_HASH)
   WOLF_HALL = hash_to_book(WOLF_HALL_HASH)
@@ -31,12 +31,12 @@ module TestData
   IF_FORTUNATE = hash_to_book(IF_FORTUNATE_HASH)
   WE = hash_to_book(WE_HASH)
   FRACTURED_SELF = hash_to_book(FRACTURED_SELF_HASH)
-  BOOKS = [DUNE, WOLF_HALL, STOIC, OTHER_PARTIES, IF_FORTUNATE, WE, FRACTURED_SELF]
+  BOOKS = [DUNE, WOLF_HALL, STOIC, OTHER_PARTIES, IF_FORTUNATE, WE, FRACTURED_SELF].freeze
 
-  WE_HASH_DUP = WE_HASH.merge({ status: 'done', formats: ['audiobook', 'ebook', 'read aloud'] })
+  WE_HASH_DUP = WE_HASH.merge({ status: 'done', formats: ['audiobook', 'ebook', 'read aloud'] }).freeze
   WE_DUP = hash_to_book(WE_HASH_DUP)
-  DUNE_HASH_DUP_1 = DUNE_HASH.merge({ status: 'done', formats: ['audiobook', 'ebook'] })
-  DUNE_HASH_DUP_2 = DUNE_HASH.merge({ status: 'done', formats: ['physical'] })
+  DUNE_HASH_DUP_1 = DUNE_HASH.merge({ status: 'done', formats: ['audiobook', 'ebook'] }).freeze
+  DUNE_HASH_DUP_2 = DUNE_HASH.merge({ status: 'done', formats: ['physical'] }).freeze
   DUNE_DUP_1 = hash_to_book(DUNE_HASH_DUP_1)
   DUNE_DUP_2 = hash_to_book(DUNE_HASH_DUP_2)
 end
