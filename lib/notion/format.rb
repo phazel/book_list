24198809759e1ffcddfc4a48081afcc644d97995
Hazel Garcia <hazelpetagarcia@gmail.com>
2021-12-31 18:10:57 +1100

    Refactor Format module

--- lib/notion/format.rb
+++ lib/notion/format.rb
@@ -1,26 +1,18 @@
 # frozen_string_literal: true
 
 module Format
+  def section(header, contents)
+    [ header, "\n", contents ].join
+  end
+
   def post(dups:, sleep:, fav:, remaining:, dnf:, current:)
-    output = [
-      "Books I Read More Than Once:\n",
-      dups,
-      "---\n\n",
-      "Books used as background noise for going to sleep:\n",
-      sleep,
-      "---\n\n",
-      "Favourites:\n",
-      fav,
-      "---\n\n",
-      "Read this year:\n",
-      remaining,
-      "---\n\n",
-      "Books I Decided Not To Finish:\n",
-      dnf,
-      "---\n\n",
-      "Currently reading:\n",
-      current,
-    ]
-    output.join
+    [
+      section("Books I Read More Than Once:", dups),
+      section("Books used as background noise for going to sleep:", sleep),
+      section("Favourites:", fav),
+      section("Read this year:", remaining),
+      section("Books I Decided Not To Finish:", dnf),
+      section("Currently reading:", current),
+    ].join("---\n\n")
   end
 end
